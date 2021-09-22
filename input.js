11f4fb93b21f9b6044005c1b0ebe8f29ae0c01ae
Edmund Yu <eyu1@ualberta.ca>
2021-09-22 22:51:19 +0000

    implemented wasd movement key functionality to our game.

--- input.js
+++ input.js
@@ -1,19 +1,33 @@
 // setup interface to handle user input from stdin
 // \u0003 maps to ctrl+c input
-const handleUserInput = (data) => {
-  if (data === "\u0003") {
-    process.exit();
-  }
-};
-const setupInput = function () {
+let connection;
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
 
+  const handleUserInput = (data) => {
+    if (data === "\u0003") {
+      process.exit();
+    } else if (data === "w") {
+      connection.write("Move: up");
+    } else if (data === "a") {
+      connection.write("Move: left");
+    } else if (data === "s") {
+      connection.write("Move: down");
+    } else if (data === "d") {
+      connection.write("Move: right");
+    }
+  };
   stdin.on("data", handleUserInput);
-
   return stdin;
 };
 
 module.exports = { setupInput };
+
+//w	87
+//a	65
+//s 83
+//d 68
