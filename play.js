1b2de3712facee6b6656791e0f3aa05eadbbc1ed
Edmund Yu <eyu1@ualberta.ca>
2021-09-22 21:41:57 +0000

    Refactored play.js moved connect function to client.js and exported the function

--- play.js
+++ play.js
@@ -1,20 +1,5 @@
 const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "135.23.223.133",
-    port: "50542",
-  });
-
-  conn.on("data", (data) => {
-    console.log(data);
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  return conn;
-};
+const { connect } = require("./client");
 
 console.log("Connecting ...");
 connect();
